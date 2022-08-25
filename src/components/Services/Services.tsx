c7caaf434891c56780b1e7d0b166adf79e7ba4c2
Cadufc91 <fernandes.cadu@gmail.com>
2022-08-25 11:06:38 -0300

    Service finalizada

---
+++ src/components/Services/Services.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import {
+    ServiceContainer,
+    ServiceTitle,
+    Divider1,
+    CardsContainer,
+    Cards,
+    CardInfo,
+    CardTitle,
+    Divider2,
+    CtaBtn,
+}from './style';
+import { BsArrowRight, BsBox, BsCode } from 'react-icons/bs';
+import { FiLayers, FiColumns } from 'react-icons/fi';
+
+const Services = () => {
+    return(
+        <ServiceContainer>
+            <ServiceTitle>My Service</ServiceTitle>
+            <Divider1 />
+            <CardsContainer>
+                <Cards>
+                    <FiLayers size={40}/>
+                    <CardInfo>
+                        <CardTitle>UI/UX Design</CardTitle>
+                        <Divider2 />
+                        <CtaBtn>Discuss now</CtaBtn>
+                        <BsArrowRight size={30} />
+                    </CardInfo>
+                </Cards>
+                <Cards>
+                    <BsBox  size={40}/>
+                    <CardInfo>
+                        <CardTitle>Product Design</CardTitle>
+                        <Divider2 />
+                        <CtaBtn>Discuss now</CtaBtn>
+                        <BsArrowRight size={30} />
+
+                    </CardInfo>
+                </Cards>
+                <Cards>
+                    <FiColumns  size={40}/>
+                    <CardInfo>
+                        <CardTitle>Branding Design</CardTitle>
+                        <Divider2 />
+                        <CtaBtn>Discuss now</CtaBtn>
+                        <BsArrowRight size={30} />
+
+                    </CardInfo>
+                </Cards>
+                <Cards>
+                    <BsCode  size={40}/>
+                    <CardInfo>
+                        <CardTitle>Front End Development</CardTitle>
+                        <Divider2 />
+                        <CtaBtn>Discuss now</CtaBtn>
+                        <BsArrowRight size={30} />
+
+                    </CardInfo>
+                </Cards>
+            </CardsContainer>
+        </ServiceContainer>
+    )
+};
+
+export default Services;
---
+++ src/components/Services/style.ts
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const ServiceContainer = styled.div`
+    margin-top: 67px;
+    margin-bottom: 80px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 468px;
+`;
+
+export const ServiceTitle = styled.h1`
+    font-size: 40px;
+    font-weight: 400;
+    color: #fff;
+`;
+
+export const Divider1 = styled.span`
+    width: 80px;
+    height: 3px;
+    background: #ED1CA6;
+    border: 1px solid #ED1CA6;
+    margin-bottom: 50px;
+`;
+
+export const CardsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    max-width: 68%;
+    height: 351px;
+`;
+
+export const Cards = styled.div`
+    padding: 22px;
+    width: 400px !important;
+    background: #fff;
+    margin-right: 24px;
+    border-radius: 6px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+export const CardInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    
+`;
+
+export const CardTitle = styled.h2`
+    margin-top: 100px;
+    margin-bottom: 16px;
+    font-size: 32px;
+    font-weight: 400;
+`;
+
+export const Divider2 = styled.span`
+    width: 64px;
+    height: 3px;
+    background: #ED1CA6;
+    border: 1px solid #ED1CA6;
+    border-radius: 50px;
+    opacity: 30%;
+    z-index: 1;
+    margin-bottom: 24px;
+`;
+
+export const CtaBtn = styled.p`
+    display: none;
+`;
